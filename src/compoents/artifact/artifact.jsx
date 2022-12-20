7239e28b66617320c980e7ecc71e98b05a54d6a6
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2022-12-20 16:34:53 +0900

    Revert "working on mui fix"
    This reverts commit dbc79a6f66138d48a06bbd2b3e34b1503f798f0c, reversing
    changes made to 4928ae5cca981b8969e1399c544bf9323429d8db.

---
+++ src/compoents/artifact/artifact.jsx
@@ -0,0 +1,30 @@
+import '../../style/component/artifact.scss';
+
+function Artifact({id, artifact, setOverlay}) {
+    artifact.eval();
+
+    return (
+        <button className="artifact" onClick={() => setOverlay({enable:true, id:id})}>
+            <h2>#{id+1}</h2>
+            <div className="score">
+                <p>AVG : {artifact.avg.score()}</p>
+                <div className="score_sub">
+                    <p>MIN : {artifact.min.score()}</p>
+                    <p>MAX : {artifact.max.score()}</p>
+                </div>
+            </div>
+            <div className="border"></div>
+            <div className="status">
+                <p>(+{artifact.level})</p>
+                <div className="status_sub">
+                    <p>{artifact.ss[0].render()}</p>
+                    <p>{artifact.ss[1].render()}</p>
+                    <p>{artifact.ss[2].render()}</p>
+                    <p>{artifact.ss[3].render()}</p>
+                </div>
+            </div>
+        </button>
+    );
+}
+
+export default Artifact;
---
+++ src/compoents/header.jsx
@@ -0,0 +1,12 @@
+import '../style/component/header.scss';
+
+function Header() {
+
+    return (
+        <header>
+            <h1>Artifact Score Simulator</h1>
+        </header>
+    );
+}
+
+export default Header;
